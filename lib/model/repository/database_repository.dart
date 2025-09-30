23a45b7ce2e5463712e3b507fcc42f2b4b802aef
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-01 00:13:54 +0100

    Created DatabaseRepository and functions to load database

---
+++ lib/model/repository/database_repository.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart' as p;
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseRepository {
+  static final DatabaseRepository _instance = DatabaseRepository._internal();
+  static Database? _database;
+  static const String _databaseName = 'timetable.db';
+
+  factory DatabaseRepository() {
+    return _instance;
+  }
+
+  DatabaseRepository._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase(_databaseName);
+    return _database!;
+  }
+
+  Future<void> _copyDatabase(String fileName) async {
+    final dbDirectory = await getDatabasesPath();
+    final path = p.join(dbDirectory, fileName);
+    if (!await File(path).exists()) {
+      ByteData data = await rootBundle.load('assets/$fileName');
+      List<int> bytes = data.buffer.asUint8List(
+        data.offsetInBytes,
+        data.lengthInBytes,
+      );
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+  }
+
+  Future<Database> _initDatabase(String fileName) async {
+    final dir = await getDatabasesPath();
+    final path = p.join(dir, fileName);
+    if (!await File(path).exists()) {
+      await _copyDatabase(fileName);
+    }
+    return await openDatabase(path);
+  }
+
+  Future<void> test() async {
+    await database;
+  }
+}
