bb0bc595248ebede5254bb5ccf51559559194ff0
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-04 12:36:26 +0100

    Created location service and function to get current location

---
+++ lib/model/services/location_service.dart
@@ -0,0 +1,50 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:railtime/model/lat_lon.dart';
+
+class LocationService {
+  /// Try to gain permission to use location from user
+  ///
+  /// Return true is location service is enabled and permitted, false otherwise
+  Future<bool> _handleLocationPermission() async {
+    // Check if location service is enabled
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return false;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return false;
+    }
+    return true;
+  }
+
+  /// Get the current GPS location
+  ///
+  /// Return [LatLon] or null no permission or unable to obtain
+  Future<LatLon?> getCurrentLocation() async {
+    // Check permission
+    final bool hasPermission = await _handleLocationPermission();
+    if (!hasPermission) {
+      return null;
+    }
+    // Get current location
+    Position location = await Geolocator.getCurrentPosition(
+      locationSettings: AndroidSettings(
+        accuracy: LocationAccuracy.medium,
+        timeLimit: Duration(seconds: 10),
+        forceLocationManager: true,
+      ),
+    );
+
+    print(location.latitude);
+    print(location.longitude);
+    return LatLon(location.latitude, location.longitude);
+  }
+}
