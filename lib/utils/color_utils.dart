b4e57782c2b06157e786294c67ef47efebb92aa9
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-07 10:51:47 +0100

    Fixed conversion error in uint24ToColor

--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class ColorUtils {
   static Color uint24ToColor(int uint24) {
-    final int red = uint24 & 0xFF0000;
-    final int green = uint24 & 0xFF00;
+    final int red = uint24 >> 16;
+    final int green = uint24 >> 8 & 0xFF;
     final int blue = uint24 & 0xFF;
 
     return Color.fromARGB(255, red, green, blue);
