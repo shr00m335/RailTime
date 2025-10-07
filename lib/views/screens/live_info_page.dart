0b966d3e43890744f45425c33da4345e5c748213
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-07 11:12:39 +0100

    Added top bar to display the closest station

--- lib/views/screens/live_info_page.dart
+++ lib/views/screens/live_info_page.dart
@@ -1,10 +1,59 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:railtime/model/station_model.dart';
+import 'package:railtime/view_model/live_info_view_model.dart';
+import 'package:railtime/views/widgets/line_icon.dart';
+import 'package:railtime/views/widgets/top_bar.dart';
 
-class LiveInfoPage extends StatelessWidget {
+class LiveInfoPage extends StatefulWidget {
   const LiveInfoPage({super.key});
 
   @override
+  State<LiveInfoPage> createState() => _LiveInfoPageState();
+}
+
+class _LiveInfoPageState extends State<LiveInfoPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<LiveInfoViewModel>(
+        context,
+        listen: false,
+      ).getNearestStation();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final StationModel? nearestStation =
+        Provider.of<LiveInfoViewModel>(context).nearestStation;
+
+    return Column(
+      children: [
+        TopBar(
+          title:
+              nearestStation != null
+                  ? Text(
+                    nearestStation.name,
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  )
+                  : SizedBox(
+                    width: 24,
+                    height: 24,
+                    child: CircularProgressIndicator(
+                      color: Theme.of(context).indicatorColor,
+                      strokeWidth: 3,
+                    ),
+                  ),
+          subtitle: Wrap(
+            children:
+                (nearestStation?.lines ?? [])
+                    .map((line) => LineIcon(line: line))
+                    .toList(),
+          ),
+        ),
+      ],
+    );
   }
 }
