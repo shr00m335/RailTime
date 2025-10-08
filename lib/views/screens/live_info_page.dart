2246696b483b42a39dba2fbb7e1a07f36f09b4a3
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-08 17:54:07 +0100

    Added page view display for Line Arrivals

--- lib/views/screens/live_info_page.dart
+++ lib/views/screens/live_info_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:railtime/model/arrival_model.dart';
 import 'package:railtime/model/station_model.dart';
 import 'package:railtime/view_model/live_info_view_model.dart';
+import 'package:railtime/views/widgets/line_arrivals.dart';
 import 'package:railtime/views/widgets/line_icon.dart';
 import 'package:railtime/views/widgets/top_bar.dart';
 
@@ -13,6 +15,25 @@ class LiveInfoPage extends StatefulWidget {
 }
 
 class _LiveInfoPageState extends State<LiveInfoPage> {
+  int _selectedLineIndex = 0;
+  final PageController _pageController = PageController();
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedLineIndex = index;
+    });
+  }
+
+  void _goToPage(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: Duration(
+        milliseconds: (_pageController.page!.floor() - index).abs() * 200,
+      ),
+      curve: Easing.linear,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -28,6 +49,8 @@ class _LiveInfoPageState extends State<LiveInfoPage> {
   Widget build(BuildContext context) {
     final StationModel? nearestStation =
         Provider.of<LiveInfoViewModel>(context).nearestStation;
+    final Map<String, List<ArrivalModel>>? arrivals =
+        Provider.of<LiveInfoViewModel>(context).arrivals;
 
     return Column(
       children: [
@@ -49,10 +72,57 @@ class _LiveInfoPageState extends State<LiveInfoPage> {
           subtitle: Wrap(
             children:
                 (nearestStation?.lines ?? [])
-                    .map((line) => LineIcon(line: line))
+                    .map((line) => LineIcon(line: line, size: 14))
                     .toList(),
           ),
         ),
+        (nearestStation != null && arrivals != null)
+            ? Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: PageView(
+                      controller: _pageController,
+                      onPageChanged: _onPageChanged,
+                      children:
+                          nearestStation.lines
+                              .map(
+                                (line) => LineArrivals(
+                                  line: line,
+                                  arrivals: arrivals[line.id] ?? [],
+                                ),
+                              )
+                              .toList(),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:
+                          nearestStation.lines
+                              .asMap()
+                              .entries
+                              .map(
+                                (entry) => GestureDetector(
+                                  onTap: () => _goToPage(entry.key),
+                                  child: LineIcon(
+                                    line: entry.value,
+                                    size:
+                                        _selectedLineIndex == entry.key
+                                            ? 16
+                                            : 12,
+                                    disabled: _selectedLineIndex != entry.key,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                    ),
+                  ),
+                ],
+              ),
+            )
+            : Expanded(child: const Center(child: CircularProgressIndicator())),
       ],
     );
   }
