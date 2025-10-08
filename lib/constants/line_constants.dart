633e7e44030401c655b40c44ca76d5e4cab4fe12
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-08 23:10:38 +0100

    Added a map to convert API direction

---
+++ lib/constants/line_constants.dart
@@ -0,0 +1,22 @@
+class LineConstants {
+  static Map<String, Map<String, int>> directions = {
+    'bakerloo': {'outbound': 0, 'inbound': 1},
+    'central': {'outbound': 0, 'inbound': 1},
+    'circle': {'outbound': 0, 'inbound': 1},
+    'district': {'outbound': 0, 'inbound': 1},
+    'hammersmith-city': {'inbound': 0, 'outbound': 1},
+    'jubilee': {'inbound': 0, 'outbound': 1},
+    'metropolitan': {'inbound': 0, 'outbound': 1},
+    'northern': {'outbound': 0, 'inbound': 1},
+    'piccadilly': {'outbound': 0, 'inbound': 1},
+    'victoria': {'outbound': 0, 'inbound': 1},
+    'waterloo-city': {'outbound': 0, 'inbound': 1},
+    'elizabeth': {'outbound': 0, 'inbound': 1},
+    'lioness': {'inbound': 0, 'outbound': 1},
+    'mildmay': {'inbound': 0, 'outbound': 1},
+    'suffragette': {'outbound': 0, 'inbound': 1},
+    'weaver': {'outbound': 0, 'inbound': 1},
+    'windrush': {'outbound': 0, 'inbound': 1},
+    'liberty': {'outbound': 0, 'inbound': 1},
+  };
+}
