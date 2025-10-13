e896cdcc9963cae2cee2b3d391884c5bfc841a9c
shr00m335 <zcabswu@ucl.ac.uk>
2025-10-13 23:23:23 +0100

    Added function to convert api map to model and is stopping field

--- lib/model/trip_model.dart
+++ lib/model/trip_model.dart
@@ -1,5 +1,6 @@
 import 'package:railtime/model/line_model.dart';
 import 'package:railtime/model/station_model.dart';
+import 'package:railtime/utils/date_time_utils.dart';
 
 class TripModel {
   final String id;
@@ -21,10 +22,11 @@ class TripModel {
 
 class TripArrivalModel {
   final StationModel station;
-  final int scheduled;
-  final int actual;
+  final int? scheduled;
+  final int? actual;
   final String? platform;
   final int sequence;
+  final bool isStopping;
 
   const TripArrivalModel(
     this.station,
@@ -32,6 +34,7 @@ class TripArrivalModel {
     this.actual,
     this.platform,
     this.sequence,
+    this.isStopping,
   );
 
   static TripArrivalModel fromDatabaseMap(
@@ -44,6 +47,26 @@ class TripArrivalModel {
       dbMap['departure_time'],
       null,
       dbMap['stop_sequence'],
+      true,
+    );
+  }
+
+  static TripArrivalModel fromApiMap(
+    Map<String, dynamic> apiMap,
+    StationModel station,
+  ) {
+    String? platform = apiMap['platformName']?.toString();
+    return TripArrivalModel(
+      station,
+      null,
+      apiMap.containsKey('expectedArrival')
+          ? DateTimeUtils.datetimeToSeconds(
+            DateTime.parse(apiMap['expectedArrival']),
+          )
+          : null,
+      platform?.split(' ').last,
+      0,
+      true,
     );
   }
 }
